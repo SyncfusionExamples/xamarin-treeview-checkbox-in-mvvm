f8e04de76f0fdbdf11eef290335b09b19473b1a0
Nijamudeenm M <nijamudeensulaiman@syncfusion.com>
2019-04-08 17:03:23 +0530

    Added sample for checkbox_bound_mode

---
+++ BoundMode_CheckBox/BoundMode_CheckBox/Model/FileManager.cs
@@ -0,0 +1,196 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Reflection;
+using System.Text;
+using Xamarin.Forms;
+using Xamarin.Forms.Internals;
+
+namespace BoundMode_CheckBox
+{
+    [Preserve(AllMembers = true)]
+    public class Folder : INotifyPropertyChanged
+    {
+        #region Fields
+
+        private string fileName;
+        private ImageSource imageIcon;
+        private ObservableCollection<File> files;
+
+        #endregion
+
+        #region Constructor
+        public Folder()
+        {
+        }
+
+        #endregion
+
+        #region Properties
+        public ObservableCollection<File> Files
+        {
+            get { return files; }
+            set
+            {
+                files = value;
+                RaisedOnPropertyChanged("SubFiles");
+            }
+        }
+
+        public string FileName
+        {
+            get { return fileName; }
+            set
+            {
+                fileName = value;
+                RaisedOnPropertyChanged("FileName");
+            }
+        }
+        public ImageSource ImageIcon
+        {
+            get { return imageIcon; }
+            set
+            {
+                imageIcon = value;
+                RaisedOnPropertyChanged("ImageIcon");
+            }
+        }
+
+        #endregion
+
+        #region INotifyPropertyChanged
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public void RaisedOnPropertyChanged(string _PropertyName)
+        {
+            if (PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(_PropertyName));
+            }
+        }
+
+        #endregion
+    }
+
+    [Preserve(AllMembers = true)]
+    public class File : INotifyPropertyChanged
+    {
+        #region Fields
+
+        private string fileName;
+        private ImageSource imageIcon;
+        private ObservableCollection<SubFile> subFiles;
+
+        #endregion
+
+        #region Constructor
+        public File()
+        {
+        }
+
+        #endregion
+
+        #region Properties
+        public ObservableCollection<SubFile> SubFiles
+        {
+            get { return subFiles; }
+            set
+            {
+                subFiles = value;
+                RaisedOnPropertyChanged("SubFiles");
+            }
+        }
+
+        public string FileName
+        {
+            get { return fileName; }
+            set
+            {
+                fileName = value;
+                RaisedOnPropertyChanged("FileName");
+            }
+        }
+        public ImageSource ImageIcon
+        {
+            get { return imageIcon; }
+            set
+            {
+                imageIcon = value;
+                RaisedOnPropertyChanged("ImageIcon");
+            }
+        }
+
+        #endregion
+
+        #region INotifyPropertyChanged
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public void RaisedOnPropertyChanged(string _PropertyName)
+        {
+            if (PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(_PropertyName));
+            }
+        }
+
+        #endregion
+    }
+
+    [Preserve(AllMembers = true)]
+    public class SubFile : INotifyPropertyChanged
+    {
+        #region Fields
+
+        private string fileName;
+        private ImageSource imageIcon;
+
+        #endregion
+
+        #region Constructor
+        public SubFile()
+        {
+        }
+
+        #endregion
+
+        #region Properties
+
+        public string FileName
+        {
+            get { return fileName; }
+            set
+            {
+                fileName = value;
+                RaisedOnPropertyChanged("FolderName");
+            }
+        }
+        public ImageSource ImageIcon
+        {
+            get { return imageIcon; }
+            set
+            {
+                imageIcon = value;
+                RaisedOnPropertyChanged("ImageIcon");
+            }
+        }
+
+        #endregion
+
+        #region INotifyPropertyChanged
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public void RaisedOnPropertyChanged(string _PropertyName)
+        {
+            if (PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(_PropertyName));
+            }
+        }
+
+        #endregion
+    }
+}
