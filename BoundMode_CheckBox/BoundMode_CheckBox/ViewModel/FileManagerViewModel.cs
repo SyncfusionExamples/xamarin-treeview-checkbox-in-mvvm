334f9270447a4db3ef658ccd33bba14fac88c3d9
Nijamudeenm M <nijamudeensulaiman@syncfusion.com>
2019-04-29 16:06:03 +0530

    View model modified

--- BoundMode_CheckBox/BoundMode_CheckBox/ViewModel/FileManagerViewModel.cs
+++ BoundMode_CheckBox/BoundMode_CheckBox/ViewModel/FileManagerViewModel.cs
@@ -21,11 +21,7 @@ namespace BoundMode_CheckBox
         }
 
         public ObservableCollection<Folder> Folders { get; set; }
-
-        public ObservableCollection<File> Files { get; set; }
-
-        public ObservableCollection<SubFile> SubFiles { get; set; }
-
+        
         public FileManagerViewModel()
         {
             this.Folders = GetFiles();
@@ -36,42 +32,36 @@ namespace BoundMode_CheckBox
             var nodeImageInfo = new ObservableCollection<Folder>();
             Assembly assembly = typeof(MainPage).GetTypeInfo().Assembly;
 
-            var doc = new Folder() { FileName = "Documents", ImageIcon = ImageSource.FromResource("SampleBrowser.SfTreeView.Icons.NodeWithImage.treeview_folder.png", assembly) };
-            var download = new Folder() { FileName = "Downloads", ImageIcon = ImageSource.FromResource("SampleBrowser.SfTreeView.Icons.NodeWithImage.treeview_folder.png", assembly) };
-            var mp3 = new Folder() { FileName = "Music", ImageIcon = ImageSource.FromResource("SampleBrowser.SfTreeView.Icons.NodeWithImage.treeview_folder.png", assembly) };
-            var pictures = new Folder() { FileName = "Pictures", ImageIcon = ImageSource.FromResource("SampleBrowser.SfTreeView.Icons.NodeWithImage.treeview_folder.png", assembly) };
-            var video = new Folder() { FileName = "Videos", ImageIcon = ImageSource.FromResource("SampleBrowser.SfTreeView.Icons.NodeWithImage.treeview_folder.png", assembly) };
+            var doc = new Folder() { FileName = "Documents"};
+            var download = new Folder() { FileName = "Downloads"};
+            var mp3 = new Folder() { FileName = "Music"};
+            var pictures = new Folder() { FileName = "Pictures"};
+            var video = new Folder() { FileName = "Videos"};
 
-            var pollution = new File() { FileName = "Environmental Pollution.docx", ImageIcon = ImageSource.FromResource("SampleBrowser.SfTreeView.Icons.NodeWithImage.treeview_word.png", assembly) };
-            var globalWarming = new File() { FileName = "Global Warming.ppt", ImageIcon = ImageSource.FromResource("SampleBrowser.SfTreeView.Icons.NodeWithImage.treeview_ppt.png", assembly) };
-            var sanitation = new File() { FileName = "Sanitation.docx", ImageIcon = ImageSource.FromResource("SampleBrowser.SfTreeView.Icons.NodeWithImage.treeview_word.png", assembly) };
-            var socialNetwork = new File() { FileName = "Social Network.pdf", ImageIcon = ImageSource.FromResource("SampleBrowser.SfTreeView.Icons.NodeWithImage.treeview_pdf.png", assembly) };
-            var youthEmpower = new File() { FileName = "Youth Empowerment.pdf", ImageIcon = ImageSource.FromResource("SampleBrowser.SfTreeView.Icons.NodeWithImage.treeview_pdf.png", assembly) };
+            var pollution = new File() { FileName = "Environmental Pollution.docx"};
+            var globalWarming = new File() { FileName = "Global Warming.ppt"};            
 
-            var games = new File() { FileName = "Game.exe", ImageIcon = ImageSource.FromResource("SampleBrowser.SfTreeView.Icons.NodeWithImage.treeview_exe.png", assembly) };
-            var tutorials = new File() { FileName = "Tutorials.zip", ImageIcon = ImageSource.FromResource("SampleBrowser.SfTreeView.Icons.NodeWithImage.treeview_zip.png", assembly) };
-            var typeScript = new File() { FileName = "TypeScript.7z", ImageIcon = ImageSource.FromResource("SampleBrowser.SfTreeView.Icons.NodeWithImage.treeview_zip.png", assembly) };
-            var uiGuide = new File() { FileName = "UI-Guide.pdf", ImageIcon = ImageSource.FromResource("SampleBrowser.SfTreeView.Icons.NodeWithImage.treeview_pdf.png", assembly) };
+            var games = new File() { FileName = "Game.exe"};
+            var tutorials = new File() { FileName = "Tutorials.zip"};
+            var typeScript = new File() { FileName = "TypeScript.7z"};
+            var uiGuide = new File() { FileName = "UI-Guide.pdf"};
 
-            var song = new File() { FileName = "Gouttes", ImageIcon = ImageSource.FromResource("SampleBrowser.SfTreeView.Icons.NodeWithImage.treeview_mp3.png", assembly) };
+            var song = new File() { FileName = "Gouttes"};
 
-            var camera = new File() { FileName = "Camera Roll", ImageIcon = ImageSource.FromResource("SampleBrowser.SfTreeView.Icons.NodeWithImage.treeview_folder.png", assembly) };
-            var stone = new File() { FileName = "Stone.jpg", ImageIcon = ImageSource.FromResource("SampleBrowser.SfTreeView.Icons.NodeWithImage.treeview_png.png", assembly) };
-            var wind = new File() { FileName = "Wind.jpg", ImageIcon = ImageSource.FromResource("SampleBrowser.SfTreeView.Icons.NodeWithImage.treeview_png.png", assembly) };
+            var camera = new File() { FileName = "Camera Roll"};
+            var stone = new File() { FileName = "Stone.jpg"};
+            var wind = new File() { FileName = "Wind.jpg"};
 
-            var img0 = new SubFile() { FileName = "WIN_20160726_094117.JPG", ImageIcon = ImageSource.FromResource("SampleBrowser.SfTreeView.Icons.NodeWithImage.treeview_img0.png", assembly) };
-            var img1 = new SubFile() { FileName = "WIN_20160726_094118.JPG", ImageIcon = ImageSource.FromResource("SampleBrowser.SfTreeView.Icons.NodeWithImage.treeview_img1.png", assembly) };
+            var img0 = new SubFile() { FileName = "WIN_20160726_094117.JPG"};
+            var img1 = new SubFile() { FileName = "WIN_20160726_094118.JPG"};
 
-            var video1 = new File() { FileName = "Naturals.mp4", ImageIcon = ImageSource.FromResource("SampleBrowser.SfTreeView.Icons.NodeWithImage.treeview_video.png", assembly) };
-            var video2 = new File() { FileName = "Wild.mpeg", ImageIcon = ImageSource.FromResource("SampleBrowser.SfTreeView.Icons.NodeWithImage.treeview_video.png", assembly) };
+            var video1 = new File() { FileName = "Naturals.mp4"};
+            var video2 = new File() { FileName = "Wild.mpeg"};
 
             doc.Files = new ObservableCollection<File>
             {
                 pollution,
-                globalWarming,
-                //sanitation,
-                //socialNetwork,
-                //youthEmpower
+                globalWarming
             };
 
             download.Files = new ObservableCollection<File>
